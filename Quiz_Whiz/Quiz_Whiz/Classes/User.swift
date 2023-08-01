4ba96e07cb532deb2752a5c6bdb196a1569cbfec
Black-Scorpio <ScorpioGentles@outlook.com>
2023-08-01 15:17:23 -0400

    starting validation for username

--- Quiz_Whiz/Quiz_Whiz/Classes/User.swift
+++ Quiz_Whiz/Quiz_Whiz/Classes/User.swift
@@ -10,29 +10,29 @@ import UIKit
 
 struct User {
     private var _name: String
-    
-    var name: String {
-        get {
-            return _name
-        }
-        set {
-            // Check if the new name contains only letters or numbers
-            let characterSet = CharacterSet.alphanumerics
-            guard newValue.rangeOfCharacter(from: characterSet.inverted) == nil else {
-                return // Invalid characters found, do not update the name
-            }
-            
-            // Check if the new name is within the maximum length of 10 characters
-            guard newValue.count <= 10 else {
-                return // Name is too long, do not update the name
-            }
-            
-            _name = newValue
-        }
-    }
-    
-    init(name: String) {
-        _name = name
-    }
+     
+     var name: String {
+         get {
+             return _name
+         }
+         set {
+             // Check if the new name contains only letters or numbers
+             let characterSet = CharacterSet.alphanumerics
+             guard newValue.rangeOfCharacter(from: characterSet.inverted) == nil else {
+                 return // Invalid characters found, do not update the name
+             }
+             
+             // Check if the new name is within the maximum length of 10 characters
+             guard newValue.count <= 10 else {
+                 return // Name is too long, do not update the name
+             }
+             
+             _name = newValue
+         }
+     }
+     
+     init(name: String) {
+         _name = name
+     }
     
 }
--- Quiz_Whiz/Quiz_Whiz/Controllers/UserViewController.swift
+++ Quiz_Whiz/Quiz_Whiz/Controllers/UserViewController.swift
@@ -20,11 +20,28 @@ class UserViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
     @IBAction func chooseTapped(_ sender: Any) {
         let categoryVC = self.storyboard?.instantiateViewController(withIdentifier: "CategoryVC") as! CategoriesViewController
-       // navigating to UserViewController
+        // Navigating to UserViewController
         self.navigationController?.pushViewController(categoryVC, animated: true)
     }
     
+    @IBAction func saveUsername(_ sender: UIButton) {
+        // Check if userNameText has a value
+        if  !userNameText.text!.isEmpty {
+            user.name = userNameText.text!
+        } else {
+            showAlert(withTitle: "Empty Name", message: "Please enter a valid username.")
+        }
+    }
+    
+    //Function to show a pop up if the user name is not a valid one
+    private func showAlert(withTitle title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        // Present the alert from the current view controller
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
