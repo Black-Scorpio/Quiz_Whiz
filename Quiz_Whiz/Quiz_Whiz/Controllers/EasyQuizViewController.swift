b4b956c26ba33f814e53bf0a889648724304fb25
Kevyn <200488106@georgianc.on.ca>
2023-08-01 21:23:47 -0400

    Code Logic 3 and Linked the final view

--- Quiz_Whiz/Quiz_Whiz/Controllers/EasyQuizViewController.swift
+++ Quiz_Whiz/Quiz_Whiz/Controllers/EasyQuizViewController.swift
@@ -44,7 +44,7 @@ class EasyQuizViewController: UIViewController {
         //testDifficulty.text = difficultyDecided
         
         // Do any additional setup after loading the view.
-        switchQuestion(currentQuestion!)
+        switchQuestion()
     
     }
     
@@ -74,36 +74,53 @@ class EasyQuizViewController: UIViewController {
     
 
     
-    func switchQuestion(_ questionNum: Int)
+    func switchQuestion()
     {
         if(categoryDecided == "Animals")
         {
-            answerButton1.setTitle(quiz.animalAnswers[questionNum][0], for: .normal);
-            answerButton2.setTitle(quiz.animalAnswers[questionNum][1], for: .normal);
-            answerButton3.setTitle(quiz.animalAnswers[questionNum][2], for: .normal);
-            answerButton4.setTitle(quiz.animalAnswers[questionNum][3], for: .normal);
+            answerButton1.setTitle(quiz.animalAnswers[currentQuestion!][0], for: .normal);
+            answerButton2.setTitle(quiz.animalAnswers[currentQuestion!][1], for: .normal);
+            answerButton3.setTitle(quiz.animalAnswers[currentQuestion!][2], for: .normal);
+            answerButton4.setTitle(quiz.animalAnswers[currentQuestion!][3], for: .normal);
         }
         else if(categoryDecided == "Programming")
         {
-            answerButton1.setTitle(quiz.codeAnswers[questionNum][0], for: .normal);
-            answerButton2.setTitle(quiz.codeAnswers[questionNum][1], for: .normal);
-            answerButton3.setTitle(quiz.codeAnswers[questionNum][2], for: .normal);
-            answerButton4.setTitle(quiz.codeAnswers[questionNum][3], for: .normal);
+            answerButton1.setTitle(quiz.codeAnswers[currentQuestion!][0], for: .normal);
+            answerButton2.setTitle(quiz.codeAnswers[currentQuestion!][1], for: .normal);
+            answerButton3.setTitle(quiz.codeAnswers[currentQuestion!][2], for: .normal);
+            answerButton4.setTitle(quiz.codeAnswers[currentQuestion!][3], for: .normal);
         }
             
             
     }
     
     
-    
-    
-    
-    
-    
+    //when buttons are pressed it will move to the next question and check score
     @IBAction func button1Pressed(_ sender: Any) {
-        progress = 1.0
+        moveToNext();
+        //checkIfCorrect();
     }
     
+    //move to next function will reset the timer and update the buttons/UI
+    func moveToNext(){
+        if(currentQuestion! < 1)
+        {
+            progress = 1.0
+            currentQuestion! += 1
+            switchQuestion()
+        }
+        else
+        {
+            //to navigate to next final score screen
+            let finalScoreVC:FinalViewController = self.storyboard?.instantiateViewController(withIdentifier: "FinalViewController") as! FinalViewController
+            //.category = categorySelected
+            //This passes the user to the difficulties view
+            //difficultiesVC.user = user
+            self.navigationController?.pushViewController(finalScoreVC, animated: true)
+        }
+    }
+    
+
 
     /*
     // MARK: - Navigation
